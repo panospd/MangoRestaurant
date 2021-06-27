833964e14261eee70d220b217f5196a87587afe8
Panos Anastasiadis <anastapd@gmail.com>
2021-06-27 11:26:56 +0100

    Implemented Product CRUD

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using Mango.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Mango.Web.Controllers
 {
