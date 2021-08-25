9944f1460d09db50b7a178f9e29cafaefaf81f8d
Panos Anastasiadis <anastapd@gmail.com>
2021-08-25 20:16:39 +0100

    Implemented OnReceiveCheckoutMessage from service bus

--- Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.OrderAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,5 +1,7 @@
 ﻿using Azure.Messaging.ServiceBus;
 using Mango.Services.OrderAPI.Messages;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Repository;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -11,12 +13,54 @@ namespace Mango.Services.OrderAPI.Messaging
 {
     public class AzureServiceBusConsumer
     {
+        private readonly IOrderRepository _orderRepository;
+
+        public AzureServiceBusConsumer(IOrderRepository orderRepository)
+        {
+            _orderRepository = orderRepository;
+        }
+
         private async Task OnCheckoutMessageReceived(ProcessMessageEventArgs args)
         {
             var message = args.Message;
             var body = Encoding.UTF8.GetString(message.Body);
 
             var checkoutHeaderDto = JsonConvert.DeserializeObject<CheckoutHeaderDto>(body);
+
+            OrderHeader orderHeader = new()
+            {
+                UserId = checkoutHeaderDto.UserId,
+                FirstName = checkoutHeaderDto.FirstName,
+                LastName = checkoutHeaderDto.LastName,
+                OrderDetails = new List<OrderDetails>(),
+                CardNumber = checkoutHeaderDto.CardNumber,
+                CouponCode = checkoutHeaderDto.CouponCode,
+                CVV = checkoutHeaderDto.CVV,
+                DiscountTotal = checkoutHeaderDto.DiscountTotal,
+                Email = checkoutHeaderDto.Email,
+                ExpiryMonthYear = checkoutHeaderDto.ExpiryMonthYear,
+                OrderTime = DateTime.Now,
+                OrderTotal = checkoutHeaderDto.OrderTotal,
+                PaymentStatus = false,
+                Phone = checkoutHeaderDto.Phone,
+                PickupDateTime = checkoutHeaderDto.PickupDateTime
+            };
+
+            foreach (var detail in checkoutHeaderDto.CartDetails)
+            {
+                OrderDetails orderDetails = new()
+                {
+                    ProductId = detail.ProductId,
+                    ProductName = detail.Product.Name,
+                    Price = detail.Product.Price,
+                    Count = detail.Count
+                };
+
+                orderHeader.CartTotalItems += detail.Count;
+                orderHeader.OrderDetails.Add(orderDetails);
+            }
+
+            await _orderRepository.AddOrder(orderHeader);
         }
     }
 }
